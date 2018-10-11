31843f3f7bb265bb47c8c21912ebe52756e3a452
TySanteOn <dante.verbauwhede@gmail.com>
2018-10-11 19:49:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, Switch, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <h1>
+            <Link to="/">TySO</Link>
+          </h1>
         </header>
+        <main>
+          <Switch>
+            <Route path="/" exact render={() => `<h2>hi</h2>`} />
+          </Switch>
+        </main>
       </div>
     );
   }
