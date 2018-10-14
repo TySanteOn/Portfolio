8959fbec8d5018acd678d744abc833fa94e9424f
TySanteOn <dante.verbauwhede@gmail.com>
2018-10-14 23:49:15 +0200

    almost finished, now only menu animation

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,8 @@ const init = () => {
     .then(parse);
 
   window.addEventListener("scroll", onScrollHandler);
+  document.querySelector(`.menu-button`).addEventListener("click", menuClickHandler);
+  document.querySelector(`.header`).addEventListener("click", menuClickHandler);
 };
 
 const makeWorkScroller =  data => {
@@ -42,6 +44,13 @@ const makeWorkScroller =  data => {
   }
 };
 
+const menuClickHandler = () => {
+  const $header = document.querySelector(`.header`);
+  // $header.style.left = 0;
+  // $header.style.opacity = 1;
+  $header.classList.toggle(`on`);
+}
+
 const parse = (data) => {
   const workData = Object.entries(data);
   works.push(workData);
@@ -49,41 +58,19 @@ const parse = (data) => {
 };
 
 const onScrollHandler = e => {
-  // const headerNav = document.querySelector(".header-ul");
-  // if (window.scrollY > 50) {
-  //   headerNav.style.backgroundColor = "#191717";
-  // } else {
-  //   headerNav.style.backgroundColor = "rgba(0,0,0,0)";
-  // };
-
-  // console.log(e);
-  // console.log(e.path[1].pageYOffset);
-
   const titles = document.querySelector(`.intro-titles`);
   const headerImg = document.querySelector(`.intro`);
   if (titles) {
     const constant = 80;
     const scrolled = e.path[1].pageYOffset / 2;
-    // console.log(scrolled);
     const newXPos = (constant + scrolled)/10;
-    // const opacity = 1 - scrolled/200;
-    // console.log(opacity);
     const backgroundXPos = 80 - scrolled/10;
 
     headerImg.style.backgroundPosition = `${backgroundXPos}%`;
 
     titles.style.left = `${newXPos}vw`;
-    // titles.style.opacity = opacity;
-    // headerImg.style.opacity = opacity;
   }
 };
-
-// const writeApp = works => {
-//   ReactDOM.render(
-//     <BrowserRouter>
-//       <App works={works} />
-//     </BrowserRouter> , document.getElementById('content'));
-// };
 
 init();
 
